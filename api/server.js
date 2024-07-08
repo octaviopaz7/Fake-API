f945acb9bba3a2c5922fd638365f8f4d0cadf1b0
Octavio Paz <octavioemanuelpaz@gmail.com>
2024-07-08 14:06:57 -0300

    cambios para permitir escritura

--- api/server.js
+++ api/server.js
@@ -1,6 +1,18 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+
+
+// Descomentar para permitir operaciones escritura
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
+
+// Comentar para permitir operaciones escritura
+//const router = jsonServer.router('db.json')
+
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
